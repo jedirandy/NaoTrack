a6134b632c60748519a13037c5cb02dab83295c2
Ma Zhuoyu <ma_zhu@calcul06.smart.metz.supelec.fr>
2013-10-21 09:23:15 +0200

    add position server

---
+++ Position/PositionServer/position-server.cc
@@ -0,0 +1,155 @@
+/*
+
+  g++ -o position-server -Wall -ansi -pedantic -O3 position-server.cc -lpthread -lboost_system-mt -std=c++11
+
+*/
+
+
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <map>
+#include <utility>
+
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <memory>
+#include <boost/asio.hpp>
+
+typedef std::pair<double,double>  Point;  // (x,y)
+typedef std::pair<int,Point>      Data;   // (label, P)
+
+class SharedValue {
+
+public:
+
+  typedef std::map<std::chrono::system_clock::time_point,Data>   time_map;
+
+private:
+
+  time_map points;
+  std::mutex lock;  
+
+public:
+
+  long int persist;
+  
+  SharedValue(void) : points(), lock(), persist(10) {}
+  ~SharedValue(void) {}
+
+  time_map operator()(void) {
+    std::unique_lock<std::mutex> exclusion(lock);
+
+    std::chrono::system_clock::time_point horizon = std::chrono::system_clock::now()-std::chrono::seconds(persist);
+    time_map::iterator begin, up;
+    begin = points.begin();
+    if(begin != points.end() && (*begin).first < horizon) {
+      up = points.upper_bound(horizon);
+      points.erase(begin,up);
+    }
+    return points;
+  }
+
+  SharedValue& operator+=(const Data& d) {
+    std::unique_lock<std::mutex> exclusion(lock);
+    points[std::chrono::system_clock::now()] = d;
+    return *this;
+  }
+
+  void clear(void) {
+    std::unique_lock<std::mutex> exclusion(lock);
+    points.clear();
+  }
+};
+
+class ServiceThread {
+private:
+
+  typedef boost::asio::ip::tcp::iostream socket_stream;
+
+  SharedValue&                      value;
+  std::shared_ptr<socket_stream>  p_socket; // Sockets streams cannot be copied....
+
+public:
+
+  ServiceThread(SharedValue& val,
+		boost::asio::ip::tcp::acceptor& acceptor) 
+    : value(val), p_socket(new socket_stream()) {
+    acceptor.accept(*(p_socket->rdbuf()));
+  }
+
+  // This is called internally at thread creation.
+  ServiceThread(const ServiceThread& cp) 
+    : value(cp.value), p_socket(cp.p_socket) {
+  }
+
+  ~ServiceThread(void) {
+  }
+
+  void operator()(void) {
+    std::string op;
+    double x,y;
+    int l;
+    socket_stream& socket = *p_socket;
+
+    try {
+      socket.exceptions(std::ios::failbit | std::ios::badbit | std::ios::eofbit);
+      while(true) {
+	socket >> op;
+	if(op == "quit") 
+	  break;
+	if(op == "clear") 
+	  value.clear();
+	else if(op == "put") {
+	  socket >> l >> x >> y;
+	  value += Data(l,Point(x,y));
+	}
+	else if(op == "get") {
+	  SharedValue::time_map::iterator iter,end;
+	  SharedValue::time_map points = value();
+	  socket << points.size() << std::endl;
+	  for(iter=points.begin(), end=points.end(); iter != end; ++iter) {
+	    Data& d = (*iter).second;
+	    Point& p = d.second;
+	    socket << d.first << ' ' << p.first << ' ' << p.second << ' ' << std::endl;
+	  }
+	}
+	else
+	  std::cerr << "Operator '" << op << "' invalid" << std::endl;
+      }
+    }
+    catch(std::exception& e) {
+      std::cout << "Exception : " << e.what() << std::endl;
+    }
+
+    std::cout << "End of thread (client disconnects)" << std::endl;
+  }
+};
+
+
+int main(int argc, char* argv[]) {
+  if(argc!=3) {
+    std::cerr << "Usage : " << argv[0] << " <port> <data persistance (seconds)>" << std::endl;
+    return 1;
+  }
+
+  try {
+    boost::asio::io_service        ios;
+    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), atoi(argv[1]));
+    boost::asio::ip::tcp::acceptor acceptor(ios, endpoint); 
+    SharedValue                    shared_value;
+
+    shared_value.persist = atoi(argv[2]);
+  
+    while(true) {
+      std::thread service(ServiceThread(shared_value,acceptor));
+      service.detach();
+    }
+  }
+  catch(std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+  
+  return 0;
+}
