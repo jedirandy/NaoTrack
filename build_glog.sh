e89df5a9819f15e76beb275421961b4cb1cbcd0f
Mato <mato@mato-VirtualBox.(none)>
2013-11-04 02:34:23 +0100

    build_glog.sh

--- build_glog.sh
+++ build_glog.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 if [ ! -d third_party/local ]
 then
     mkdir third_party/local
@@ -6,8 +6,9 @@ fi
 
 cd third_party/local
 PREFIX_PATH=`pwd`
+cd ..
 
-if [ ! -d third_party/glog]
+if [ ! -d glog ]
 then
     svn checkout http://google-glog.googlecode.com/svn/trunk/ glog
     cd glog
